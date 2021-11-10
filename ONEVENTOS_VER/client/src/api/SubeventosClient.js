36bae34f294b059367ff91fe9f6ff2717a3a306d
Rodolfo <rodolfiux1@hotmail.com>
2021-11-10 02:04:38 -0600

    Editar y consultar subeventos query

---
+++ ONEVENTOS_VER/client/src/api/SubeventosClient.js
@@ -0,0 +1,25 @@
+import Config from '@/api/config';
+import axios from 'axios';
+
+export default class SubeventosClient {
+	/**
+	 * Regresa todos los subeventos
+	 * @return {Object[]} Un array de subeventos
+	 */
+	static async getAllSubeventos() {
+		try {
+			const response = await axios.get(`${Config.BASE_URL}/api/subeventos`);
+			if (response.status != 200) {
+				throw new Error(response.data)
+			} else {
+				return response.data;
+			}
+		} catch (e) {
+			console.log(e);
+			return [];
+		}
+	}
+
+
+	
+}

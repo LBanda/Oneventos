b9ebdad2e3ebfd7285505aaace4dcd9ec399c022
Rodolfo <rodolfiux1@hotmail.com>
2021-11-29 16:22:18 -0600

    Controller Editar y eliminar Subevento

--- ONEVENTOS_VER/server/controllers/subeventosController.js
+++ ONEVENTOS_VER/server/controllers/subeventosController.js
@@ -20,6 +20,20 @@ router.post("/api/subeventos/", async (req, res) => {
     }
 })
 
+router.delete("/api/subevento/:id/borrar", async (req, res )=>{
+    const { id } = req.params;
+    const subevento = await SubEvento.deleteSubevento(id);
+    res.status(200).json(subevento ?? {});
+  
+})
+
+router.put("api/subevento/:id/editar", async (req, res) => {
+    const {id} = req.params; 
+    const subevento = SubEvento.editSubeventoById(id);
+    res.status(200).json(subevento ?? []);
+
+})
+
 
 module.exports = {
     subEventosRoutes: router
